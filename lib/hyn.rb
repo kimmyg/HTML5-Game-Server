bab4e7be09f9bfb8fc69d2529da8f02790f65434
Kimball Germane <krgermane@yahoo.com>
2012-04-30 14:14:36 -0600

    hyn stuff

---
+++ lib/hyn.rb
@@ -0,0 +1,32 @@
+=begin
+
+the game has two states: standard and finalization
+
+in standard, a turn consists of one of:
+discard
+meld
+challenge
+
+in finalization, a turn consists of one of:
+pass
+meld
+challenge
+
+the game ends when everyone passes in a row
+
+once the option is chosen
+
+discard:
+tap one of the cards in hand to discard, message is sent, draw automatically
+cancel button
+
+meld:
+tap two of cards in hand plus discard.
+cancel button
+
+challenge:
+tap one of cards in hand plus discard and tap card in hand.
+- each constrains the other, reflect this.
+cancel button (even after one of the cards has been selected.)
+
+=end
